d624e3cd06899a9d6099fae64dfae6dbdc592706
Russell Osborne <russell@burningpony.com>
2016-05-17 17:28:10 -0400

    Use key when getting meetups

--- lib/kiosk/worthwhile_meetup.rb
+++ lib/kiosk/worthwhile_meetup.rb
@@ -1,5 +1,5 @@
 class WorthwhileMeetup < Meetup
   def url
-    "https://api.meetup.com/2/events?member_id=#{ENV['PRIMARY_MEETUP_MEMBER_ID']}&offset=0&format=json&limited_events=False&photo-host=public&page=20&fields=&order=time&desc=false&status=upcoming&sig_id=#{ENV['PRIMARY_MEETUP_MEMBER_SIG_ID']}&sig=#{ENV['PRIMARY_MEETUP_MEMBER_SIG']}"
+    "https://api.meetup.com/2/events?member_id=#{ENV['PRIMARY_MEETUP_MEMBER_ID']}&offset=0&format=json&limited_events=False&photo-host=public&page=20&fields=&order=time&desc=false&status=upcoming&sign=true&key=#{ENV['PRIMARY_MEETUP_API_KEY']}"
   end
 end
